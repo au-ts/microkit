51b422eb80f8439232be2e5f9a6aa5cc8d498091
Bill Nguyen <bill.nguyen@student.unsw.edu.au>
2025-07-14 16:46:57 +1000

    example/x86_64_ioport: add large page and self name testing
    Signed-off-by: Bill Nguyen <bill.nguyen@student.unsw.edu.au>

--- example/x86_64_ioport/hello.c
+++ example/x86_64_ioport/hello.c
@@ -10,6 +10,10 @@ uintptr_t small_memory_region_vaddr;
 uintptr_t small_memory_region_paddr;
 uint64_t small_memory_region_size;
 
+uintptr_t large_memory_region_vaddr;
+uintptr_t large_memory_region_paddr;
+uint64_t large_memory_region_size;
+
 static char hexchar(unsigned int v)
 {
     return v < 10 ? '0' + v : ('a' - 10) + v;
@@ -30,7 +34,9 @@ static void puthex64(uint64_t val)
 
 void init(void)
 {
-    microkit_dbg_puts("hello, world\n");
+    microkit_dbg_puts("hello, world. my name is ");
+    microkit_dbg_puts(microkit_name);
+    microkit_dbg_puts("\n");
 
     microkit_dbg_puts("small region vaddr: ");
     puthex64(small_memory_region_vaddr);
@@ -44,11 +50,33 @@ void init(void)
     puthex64(small_memory_region_size);
     microkit_dbg_puts("\n");
 
-    microkit_dbg_puts("writing data to vaddr...");
-    char *c = (char *) small_memory_region_vaddr;
+    microkit_dbg_puts("writing data to end of small region...");
+    char *c = (char *) (small_memory_region_vaddr + 0xfff);
     *c = 's';
     microkit_dbg_puts("reading back...");
     if (*c == 's') {
+        microkit_dbg_puts("OK\n");
+    } else {
+        microkit_dbg_puts("FAIL\n");
+    }
+
+    microkit_dbg_puts("large region vaddr: ");
+    puthex64(large_memory_region_vaddr);
+    microkit_dbg_puts("\n");
+
+    microkit_dbg_puts("large region paddr: ");
+    puthex64(large_memory_region_paddr);
+    microkit_dbg_puts("\n");
+
+    microkit_dbg_puts("large region size: ");
+    puthex64(large_memory_region_size);
+    microkit_dbg_puts("\n");
+
+    microkit_dbg_puts("writing data to end of large region...");
+    c = (char *) (large_memory_region_vaddr + 0x1fffff);
+    *c = 'e';
+    microkit_dbg_puts("reading back...");
+    if (*c == 'e') {
         microkit_dbg_puts("OK\n");
     } else {
         microkit_dbg_puts("FAIL\n");
