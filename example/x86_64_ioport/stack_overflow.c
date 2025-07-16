cbee4c80b0b07edb2eba8827574f91fbdb7d492e
Bill Nguyen <bill.nguyen@student.unsw.edu.au>
2025-07-16 16:10:15 +1000

    example/x86_64_ioport: make crash overflow PD work correctly at -O3
    Signed-off-by: Bill Nguyen <bill.nguyen@student.unsw.edu.au>

--- example/x86_64_ioport/stack_overflow.c
+++ example/x86_64_ioport/stack_overflow.c
@@ -6,9 +6,17 @@
 #include <stdint.h>
 #include <microkit.h>
 
-void recurse(void) {
-    char useless[16];
+__attribute__((noinline, noclone))
+void recurse() {
+    microkit_dbg_puts("recurse()\n");
+    
+    volatile char useless[1024];
+    for (int i = 0; i < 1024; i++) {
+        useless[i] = (char) 0;
+    }
+
     recurse();
+    useless[0] = (char) 0;
 }
 
 void
@@ -16,7 +24,7 @@ init(void)
 {
     microkit_dbg_puts("STACK OVERFLOW PD STARTING\n");
     microkit_dbg_puts("STACK OVERFLOW PD GOING OFF NOW!!!!!!!!\n");
-    recurse();
+    recurse((char *) 0);
 }
 
 void
