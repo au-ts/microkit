aab5de67060e695b895c4a48fb5c454ca50a2535
Bill Nguyen <bill.nguyen@student.unsw.edu.au>
2025-07-29 17:13:54 +1000

    example/x86_64_ioport: simplify
    Signed-off-by: Bill Nguyen <bill.nguyen@student.unsw.edu.au>

--- example/x86_64_ioport/README.md
+++ example/x86_64_ioport/README.md
@@ -5,7 +5,7 @@
 # Example - Hello World
 
 This is a basic hello world example that has a single protection domain
-that simply prints "hello, world!" via the serial I/O port (0x03F8) upon initialisation.
+that simply prints "hello, world!" via the first serial I/O port (0x3F8) upon initialisation.
 
 Only x86_64 platforms are supported.
 
---
+++ example/x86_64_ioport/x86_64_ioport.c
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2025, UNSW
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+#include <stdint.h>
+#include <microkit.h>
+
+#define SERIAL_IOPORT_ID 0
+
+static inline void serial_putc(char ch)
+{
+    microkit_x86_ioport_write_8(SERIAL_IOPORT_ID, 0x3f8, ch);
+}
+
+static inline void serial_puts(const char *s)
+{
+    while (*s) {
+        if (*s == '\n')
+            serial_putc('\r');
+        serial_putc(*s++);
+    }
+}
+
+void init(void)
+{
+    microkit_dbg_puts("hello, world. my name is ");
+    microkit_dbg_puts(microkit_name);
+    microkit_dbg_puts("\n");
+
+    microkit_dbg_puts("Now writing to serial I/O port: ");
+    serial_puts("hey there from serial I/O port\n");
+}
+
+void notified(microkit_channel ch)
+{
+}
