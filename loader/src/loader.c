fa1a64caa51f8466ed3e324ace6760f10059db29
Jakub Duchniewicz <j.duchniewicz@unsw.edu.au>
2025-10-22 16:18:49 +1100

    Initial support for rock3b.
    Signed-off-by: Jakub Duchniewicz <j.duchniewicz@unsw.edu.au>

--- loader/src/loader.c
+++ loader/src/loader.c
@@ -306,6 +306,20 @@ static void putc(uint8_t ch)
     *UART_REG(UTHR) = ch;
 }
 
+#elif defined(BOARD_rock3b)
+#define UART_BASE   0xfdd50000
+#define UTHR        0x0
+#define ULSR        0x14
+#define ULSR_THRE   (1 << 5)
+
+static void uart_init() {}
+
+static void putc(uint8_t ch)
+{
+    while ((*UART_REG(ULSR) & ULSR_THRE) == 0);
+    *UART_REG(UTHR) = ch;
+}
+
 #elif defined(ARCH_riscv64)
 #define SBI_CONSOLE_PUTCHAR 1
 
