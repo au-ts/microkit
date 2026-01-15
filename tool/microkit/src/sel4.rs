b4626f616506e73acb6ab9fe62050a1c6fa61537
Krishnan Winter <krishnan.winter@unsw.edu.au>
2026-01-15 14:49:47 +1100

    add sel4 boot info structures
    Signed-off-by: Krishnan Winter <krishnan.winter@unsw.edu.au>

--- tool/microkit/src/sel4.rs
+++ tool/microkit/src/sel4.rs
@@ -25,6 +25,51 @@ pub struct BootInfo {
     pub first_available_cap: u64,
 }
 
+#[repr(C, packed)]
+#[derive(Debug, Copy, Clone)]
+pub struct seL4_KernelBootInfo {
+    pub magic: u32,
+    pub version: u8,
+    pub _padding0: [u8; 3usize],
+    pub root_task_entry: u64,
+    pub num_kernel_regions: u8,
+    pub num_ram_regions: u8,
+    pub num_root_task_regions: u8,
+    pub num_reserved_regions: u8,
+    pub num_mpidrs: u8,
+    pub _padding: [u8; 3usize],
+}
+#[repr(C, packed(8))]
+#[derive(Debug, Copy, Clone)]
+pub struct seL4_KernelBoot_KernelRegion {
+    pub base: u64,
+    pub end: u64,
+}
+#[repr(C, packed(8))]
+#[derive(Debug, Copy, Clone)]
+pub struct seL4_KernelBoot_RamRegion {
+    pub base: u64,
+    pub end: u64,
+}
+#[repr(C, packed(8))]
+#[derive(Debug, Copy, Clone)]
+pub struct seL4_KernelBoot_RootTaskRegion {
+    pub paddr_base: u64,
+    pub paddr_end: u64,
+    pub vaddr_base: u64,
+    pub _padding: [u8; 8usize],
+}
+#[repr(C, packed(8))]
+#[derive(Debug, Copy, Clone)]
+pub struct seL4_KernelBoot_ReservedRegion {
+    pub base: u64,
+    pub end: u64,
+}
+
+pub const SEL4_KERNEL_BOOT_INFO_MAGIC: u32 = 0x73654c34;  /* "seL4" */
+
+pub const SEL4_KERNEL_BOOT_INFO_VERSION_0: u8 = 0;        /* Version 0 */
+
 fn kernel_self_mem(kernel_elf: &ElfFile) -> MemoryRegion {
     let segments = kernel_elf.loadable_segments();
     let base = segments[0].phys_addr;
