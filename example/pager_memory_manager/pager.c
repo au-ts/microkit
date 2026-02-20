c43d1f6696560aebf95cff4cf73e235bfac48e2c
Joshua Shim <joshua.shim2004@gmail.com>
2026-02-20 16:53:54 +1100

    dkfjs

--- example/pager_memory_manager/pager.c
+++ example/pager_memory_manager/pager.c
@@ -25,8 +25,12 @@ static uint64_t num_frames;
 
 typedef struct FrameInfo {
     Cap cap;
-    uint32_t frame;
-    bool ws;
+    uint32_t frame_id;
+    uint64_t last_accessed; // for working set.
+    // bool dirty; // probably better to keep a bool instead of a pointer to the shadow page table
+    pe *page; // the page this frame is mapped to.
+    uint32_t next;
+    uint32_t prev;
 } FrameInfo;
 
 // pub struct Rights {
@@ -62,6 +66,8 @@ typedef struct microkit_data {
     uintptr_t pd_idx;
 } frame_pd_id;
 
+
+
 frame_pd_id *frames = (frame_pd_id *) unmapped_frames_addr;
 
 
@@ -84,12 +90,28 @@ typedef struct page_upper_directory {
 } pud;
 
 
-
 // stuff required for the vm fault handling
 pud pgd[MAX_PDS][NUM_PT_ENTRIES]; // page tables for the children.
-frame_pd_id *frame_table[MAX_PDS];
+
+
+FrameInfo frame_table[MAX_PDS]; // this functions as a doubly ll.
+
+
 // TODO: have process vspace ptrs here as well.
 unsigned long vspaces[MAX_PDS];
+unsigned long long time = 0; // Working set clock.
+#define TAU 10 // not too sure what the optimal number for this would be.
+
+#define PGD_INDEX(va) (((va) >> 39) & 0x1FF)
+#define PUD_INDEX(va) (((va) >> 30) & 0x1FF)
+#define PD_INDEX(va)  (((va) >> 21) & 0x1FF)
+#define PT_INDEX(va)  (((va) >> 12) & 0x1FF)
+
+static retrieve_page(uintptr_t fault_addr, uint32_t pd_idx) {
+    if (!pgd[pd_idx][PGD_INDEX(fault_addr)]) {
+        
+    }
+}
 
 void init(void)
 {
@@ -99,11 +121,11 @@ void init(void)
     // need to initialise these
 
     for (int i = 0; i < num_frames; ++i) {
-        // fill in the frame table
-        frame_table[frames[i].pd_idx] = &frames[i];
-        // TODO: fill process vspaces.
+        int next = i + 1, prev = i - 1;
+        if (i == 0) prev = num_frames - 1;
+        if (i == num_frames - 1) next = 0;
+        frame_table[frames[i].pd_idx] = {frames[i].frame_cap, frames[i].frame_id, 0, false, next, prev};
     }
-
 }
 
 void notified(microkit_channel ch)
@@ -118,9 +140,15 @@ seL4_MessageInfo_t protected(microkit_channel ch, microkit_msginfo msginfo)
 
 seL4_Bool fault(microkit_child child, microkit_msginfo msginfo, microkit_msginfo *reply_msginfo)
 {
+    ++time;
     // TODO: this is when the child has a vm fault...
-    uint64_t fault_addr = microkit_mr_get(1); // I am not sure if this is the right mr number so will need to check later.
-    // i return nothing here.
+    uintptr_t fault_addr = microkit_mr_get(1); // I am not sure if this is the right mr number so will need to check later.
+    // check if a page in is required.
+    
+    // check if page out is required.
+
+    // find a unused frame and map
+
     
 }
 
