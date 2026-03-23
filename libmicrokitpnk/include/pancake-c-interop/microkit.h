20f9b91b2ce394779cdcf2f426826e132d9873e2
Zoltan A. Kocsis <11808286+zaklogician@users.noreply.github.com>
2026-03-23 05:59:44 +0000

    pancake: remove weak protected from microkit.h
    Signed-off-by: Zoltan A. Kocsis <11808286+zaklogician@users.noreply.github.com>

--- libmicrokitpnk/include/pancake-c-interop/microkit.h
+++ libmicrokitpnk/include/pancake-c-interop/microkit.h
@@ -133,18 +133,10 @@ static inline seL4_Word microkit_mr_get(seL4_Uint8 mr)
     return seL4_GetMR(mr);
 }
 
-__attribute__((weak)) seL4_Bool fault(microkit_child child, microkit_msginfo msginfo, microkit_msginfo *reply_msginfo)
+seL4_Bool fault(microkit_child child, microkit_msginfo msginfo, microkit_msginfo *reply_msginfo)
 {
     microkit_dbg_puts(microkit_name);
     microkit_dbg_puts(" is missing the 'fault' entry point, unsupported for C PDs on Pancake Microkit\n");
     microkit_internal_crash(0);
     return seL4_False;
-}
-
-__attribute__((weak)) microkit_msginfo protected(microkit_channel ch, microkit_msginfo msginfo)
-{
-    microkit_dbg_puts(microkit_name);
-    microkit_dbg_puts(" is missing the 'protected' entry point\n");
-    microkit_internal_crash(0);
-    return seL4_MessageInfo_new(0, 0, 0, 0);
 }
